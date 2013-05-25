784142e9c3fc0324dd0de094570d5efe1e3e46ac
Dario Bertini <berdario@gmail.com>
2013-05-26 01:22:01 +0200

    Updated setup.py, version 0.1.3

--- setup.py
+++ setup.py
@@ -3,10 +3,18 @@
 from setuptools import setup
 import invewrapper
 
+try:
+    with open('README.md') as readme:
+        long_desc = readme.read()
+except IOError:
+    long_desc = ''
+
 setup(
 	name='invewrapper',
-	version='0.1.2',
+	version='0.1.3',
 	description='tools to manage multiple virtualenv written in pure python',
+	long_description=long_desc,
+	keywords='virtualenvwrapper',
 	author='Dario Bertini',
 	author_email='berdario+pypi@gmail.com',
 	url='https://github.com/berdario/invewrapper',
@@ -17,5 +25,10 @@ setup(
 	entry_points={
 		'console_scripts':
 			["{0} = invewrapper.invewrapper:{0}_cmd".format(cmd[:-4])
-			for cmd in dir(invewrapper.invewrapper) if cmd.endswith("_cmd")]}
+			for cmd in dir(invewrapper.invewrapper) if cmd.endswith("_cmd")]},
+	classifiers=[
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 3',
+		'Intended Audience :: Developers',
+		'Environment :: Console']
 )
