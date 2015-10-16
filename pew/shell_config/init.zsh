570cf9ae496049a5f40f2139c5e180d347bb98a0
Dario Bertini <berdario@gmail.com>
2015-10-16 18:00:41 +0100

    Remove colors from init.zsh (it was somehow breaking the shell completion)
    Fixes #69

--- pew/shell_config/init.zsh
+++ pew/shell_config/init.zsh
@@ -1,9 +1,12 @@
 fpath=( ${0:a:h} "${fpath[@]}" )
 compinit
 
-autoload -U colors && colors
-
-_venv=$(basename "$VIRTUAL_ENV")
-PS1="%{$fg_bold[blue]%}$_venv${_venv:+ }$reset_color$PS1"
+function virtualenv_prompt_info() {
+    if [ -n "$VIRTUAL_ENV" ]; then
+        local name=$(basename $VIRTUAL_ENV)
+        echo "($name)"
+    fi
+}
+PS1="$(virtualenv_prompt_info)$PS1"
 
 # be sure to disable promptinit if the prompt is not updated
