695d9afea7b2c7738f9ac1d9704c25720e67a781
anatoly techtonik <techtonik@gmail.com>
2016-03-01 00:17:19 +0300

    No need to create .local/share on Windows

--- pew/pew.py
+++ pew/pew.py
@@ -40,11 +40,13 @@ if sys.version_info[0] == 2:
 
 err = partial(print, file=sys.stderr)
 
+if windows:
+    default_home = '~/.virtualenvs'
+else:
+    default_home = os.path.join(
+        os.environ.get('XDG_DATA_HOME', '~/.local/share'), 'virtualenvs')
 workon_home = expandpath(
-    os.environ.get('WORKON_HOME',
-                   os.path.join(os.environ.get('XDG_DATA_HOME',
-                                               '~/.local/share'),
-                                'virtualenvs')))
+    os.environ.get('WORKON_HOME', default_home))
 
 
 def makedirs_and_symlink_if_needed(workon_home):
