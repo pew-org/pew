592a59e2d1b7377cb31d2d1a30e921726874c618
Cimon Lucas (LCM) <LCimon@voyages-sncf.com>
2016-07-06 09:58:15 +0200

    Bugfix: pew in/workon /absolute/path (fix: #108)

--- tests/test_install.py
+++ tests/test_install.py
@@ -7,7 +7,7 @@ import pytest
 
 
 @skip_windows(reason='Pythonz is unsupported')
-@pytest.mark.skipif(sys.version_info > (2,7) and os.environ.get('CI') == 'true',
+@pytest.mark.skipif(sys.platform == 'cygwin' or (sys.version_info > (2,7) and os.environ.get('CI') == 'true'),
                     reason='Limit this slow and expensive test to the oldest '
                     'Python version in the CI environment')
 def test_install():
