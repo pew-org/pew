4782a60b63ac8e557175fca825e6390546f72bf5
Dario Bertini <berdario@gmail.com>
2016-08-29 17:43:29 +0100

    Add message when entering nested environments, Fix #98

--- pew/pew.py
+++ pew/pew.py
@@ -143,7 +143,9 @@ def fork_shell(env, shellcmd, cwd):
     or_ctrld = '' if windows else "or 'Ctrl+D' "
     err("Launching subshell in virtual environment. Type 'exit' ", or_ctrld,
         "to return.", sep='')
-
+    if 'VIRTUAL_ENV' in os.environ:
+        err("Be aware that this environment will be nested on top "
+            "of '%s'" % Path(os.environ['VIRTUAL_ENV']).name)
     inve(env, *shellcmd, cwd=cwd)
 
 
