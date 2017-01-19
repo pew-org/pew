ea96742197360f86da016dfad3b850f77fab13f8
Dario Bertini <berdario@gmail.com>
2017-01-19 02:08:22 +0000

    Add README to tarball, bump to 0.1.25

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ https://github.com/berdario/pew#usage'''
 
 setup(
     name='pew',
-    version='0.1.24',
+    version='0.1.25',
     description='tool to manage multiple virtualenvs written in pure python',
     long_description=long_desc,
     author='Dario Bertini',
