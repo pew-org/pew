5b941b444118ae225c12de886ad8edbad188d00b
Al Johri <al.johri@gmail.com>
2017-09-20 20:18:54 -0400

    add space in correct spot

--- pew/shell_config/init.zsh
+++ pew/shell_config/init.zsh
@@ -4,9 +4,9 @@ compinit
 function virtualenv_prompt_info() {
     if [ -n "$VIRTUAL_ENV" ]; then
         local name=$(basename $VIRTUAL_ENV)
-        echo "($name)"
+        echo "($name) "
     fi
 }
-PS1="$(virtualenv_prompt_info) $PS1"
+PS1="$(virtualenv_prompt_info)$PS1"
 
 # be sure to disable promptinit if the prompt is not updated
