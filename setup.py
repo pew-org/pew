600d97a5d90303cf6dd049a9111f55dc7cf0e651
Dario Bertini <berdario@gmail.com>
2017-10-06 02:56:54 +0100

    Bump to 1.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ https://github.com/berdario/pew#usage'''
 
 setup(
     name='pew',
-    version='0.1.27',
+    version='1.0.0',
     description='tool to manage multiple virtualenvs written in pure python',
     long_description=long_desc,
     author='Dario Bertini',
