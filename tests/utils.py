20f6a8d2c240565aa32bb790ee3eb854bad10e0c
Michal Cyprian <m.cyprian@gmail.com>
2017-12-13 14:11:31 +0100

    Add webtest marker to skip tests requiring connection if not available

--- tests/utils.py
+++ tests/utils.py
@@ -11,12 +11,28 @@ except ImportError:
         yield tmpdir
         rmtree(tmpdir)
 
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+
 import functools
 import os
 from sys import platform
 
 import pytest
 
+
+def has_connection():
+    try:
+        urlopen('https://pypi.org/')
+    except OSError:
+        return False
+    return True
+
+
 skip_windows = functools.partial(pytest.mark.skipif, platform == 'win32')
 xfail_nix = pytest.mark.xfail(os.environ.get('NIX'),
                               reason="Test not yet working while building in Nix")
+skip_webtest = pytest.mark.skipif(has_connection() is False,
+                                  reason="Test requires an internet connection")
