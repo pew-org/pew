5ad86296e4a7ac596406283d1f3055199dea0209
Dan Ryan <dan@danryan.co>
2018-03-08 17:12:12 -0500

    Dont rely on grandparent process to be shell
    - Change function to `get_shell`
    - Allow `get_shell` to take `max_depth` as an arg

--- pew/pew.py
+++ pew/pew.py
@@ -36,7 +36,7 @@ else:
     InstallCommand = ListPythons = LocatePython = UninstallCommand = \
         lambda : sys.exit('Command not supported on this platform')
 
-    from ._win_utils import get_grandparent_process
+    from ._win_utils import get_shell
 
 from pew._utils import (check_call, invoke, expandpath, own, env_bin_dir,
                         check_path, temp_environ, NamedTemporaryFile, to_unicode)
@@ -184,7 +184,7 @@ def _detect_shell():
         if 'CMDER_ROOT' in os.environ:
             shell = 'Cmder'
         elif windows:
-            shell = get_grandparent_process(os.getpid())
+            shell = get_shell(os.getpid())
         else:
             shell = 'sh'
     return shell
