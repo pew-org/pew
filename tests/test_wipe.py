b060368e5d3886920b33426bec5fd4afda36088b
Dario Bertini <berdario@gmail.com>
2018-03-27 03:21:14 +0100

    Checking stderr contents for a test wasn't a bright idea

--- tests/test_wipe.py
+++ tests/test_wipe.py
@@ -5,7 +5,7 @@ from utils import connection_required
 
 @connection_required
 def test_wipe(env1):
-    assert not invoke('in', 'env1', 'pip', 'install', 'WebTest').err
+    assert invoke('in', 'env1', 'pip', 'install', 'WebTest').returncode == 0
     assert 'WebTest' in invoke('in', 'env1', 'pip', 'freeze').out
     invoke('wipeenv', 'env1')
     assert 'WebTest' not in invoke('in', 'env1', 'pip', 'freeze').out
