9c657e719c9bca8d73d821957cf52a515ee5f125
Benoit Brayer <brayer@alma.fr>
2023-11-17 09:27:56 +0100

    Use fork to fix install of python versions with minor version containing more than one digit (>= 3.10.X).

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
             'shellingham'
         ],
         'pythonz': [
-            'pythonz-bd>=1.10.2'
+            'pythonz-bd @ git+https://github.com/M0dM/pythonz.git@bd'
         ]
     },
     include_package_data=True,
